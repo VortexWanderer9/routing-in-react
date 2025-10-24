33dd7c926c77e00656d948d14b762ec367edaa29
Tyrell Wellick <keshavniure99@gmail.com>
2025-10-24 09:48:23 -0800

    Add footer component with logo, social links, and copyright information

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,29 @@ import React from 'react'
 function Footer() {
   return (
     <>
+   <div className=' flex flex-col'>
+    <div className="logo">
+      <img className='cursor-pointer' src="./panther.svg" alt="" />
+    </div>
+    <div className="links flex justify-around items-center">
+      <div className="social-links flex flex-col">
+        <span className="font-bold">Social Links</span>
+      <a href="#">YouTube</a>
+      <a href="#">GitHub</a>
+      <a href="#">Facebook</a>
+    </div>
+    <div className="page-links flex flex-col">
+      <span className="font-bold">Page Links</span>
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Contract</a>
+    </div>
+    </div>
+    <div className="copyright flex justify-center items-center p-4  text-black font-mono font-semibold bg-emerald-600 mt-6">
+      <span >© 2025 React Project. All rights reserved.</span>
+
+    </div>
+   </div>
     </>
   )
 }
